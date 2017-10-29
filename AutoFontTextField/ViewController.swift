7695804b92e0efabf39b07aeaad92f288e09e666
Barlow Tucker <barlowtucker@gmail.com>
2017-10-28 22:16:05 -0600

    Initial Commit

---
+++ AutoFontTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoFontTextField
+//
+//  Created by Barlow Tucker on 10/28/17.
+//  Copyright © 2017 Barlow Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
